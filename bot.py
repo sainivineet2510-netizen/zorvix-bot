7a56ed8f685865de63f7cd8d854b31e1dd4152cf
sainivineet2510-netizen <sainivineet2510@gmail.com>
2026-05-12 19:10:09 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import os
 
-TOKEN = os.getenv(:AAEMW7i5hXc-VIHnIEdDV1FbWghDNW7Qn5U)
+TOKEN = os.getenv("TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Zorvix Bot is Online 🚀")
