4a84c2371a6eacdb9596595c3e1f4874d0753acf
sainivineet2510-netizen <sainivineet2510@gmail.com>
2026-05-12 18:36:22 +0530

    Correct token retrieval in bot.py
    Fix the environment variable retrieval for the bot token.

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import os
 
-TOKEN = os.getenv(8769943613:AAEMW7i5hXc-VIHnIEdDV1FbWghDNW7Qn5U)
+TOKEN = os.getenv(:AAEMW7i5hXc-VIHnIEdDV1FbWghDNW7Qn5U)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Zorvix Bot is Online 🚀")
